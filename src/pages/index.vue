736eaa3dfa66dc1cbac92085290c0f3a75a38c6f
jasonwu513 <jasonwu513@gmail.com>
2021-12-27 22:17:53 +0800

    install unplugin-icons

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -2,5 +2,18 @@
     <h1>
         Home
     </h1>
+    <!-- <HelloWorld msg="HiHi"/> -->
+    <!-- <IconHome class="w-8 h-8 text-violet-600" /> -->
+    <heroicons-outline-user class="w-8 h-8 text-violet-600" />
 </template>
+
+<script>
+    // import IconHome from '~icons/heroicons-outline/home'
+
+    // export default {
+    //     components: {
+    //         IconHome,
+    //     },
+    // }
+</script>
 
--- vite.config.js
+++ vite.config.js
@@ -2,12 +2,25 @@ import path from 'path'
 import { defineConfig } from 'vite'
 import Vue from '@vitejs/plugin-vue'
 import Pages from 'vite-plugin-pages'
+import Components from 'unplugin-vue-components/vite'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
+
+
+
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     Vue(),
     Pages(),
+    Components({
+      resolvers: IconsResolver({
+        prefix: false,
+        enabledCollections: ['heroicons-outline'],
+      }),
+    }),
+    Icons(),
   ],
   server: {
     port : 3333
